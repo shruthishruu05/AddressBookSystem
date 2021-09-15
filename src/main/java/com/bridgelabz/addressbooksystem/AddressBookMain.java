f0c8bcd6e3d0063cd669c391ddca6819d1d7d356
shruthishruu05 <shruthi.m@perfios.com>
2021-09-15 14:32:49 +0530

    ADDED : welcome message

--- src/main/java/com/bridgelabz/addressbooksystem/AddressBookMain.java
+++ src/main/java/com/bridgelabz/addressbooksystem/AddressBookMain.java
@@ -1,5 +1,8 @@
 package com.bridgelabz.addressbooksystem;
 
-public class AddressBookMain {
+	public class AddressBookMain {
+		public static void main(String [] args) {
+			System.out.println("welcome to address book");
+		}
+	}
 
-}

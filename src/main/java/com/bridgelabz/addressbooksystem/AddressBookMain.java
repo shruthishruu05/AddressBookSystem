3e2782fa2153ef96e14d7ea79fa46734a3443a50
shruthishruu05 <shruthi.m@perfios.com>
2021-09-15 14:57:54 +0530

    ADDED : calling method to add contact details

--- src/main/java/com/bridgelabz/addressbooksystem/AddressBookMain.java
+++ src/main/java/com/bridgelabz/addressbooksystem/AddressBookMain.java
@@ -3,6 +3,9 @@ package com.bridgelabz.addressbooksystem;
 	public class AddressBookMain {
 		public static void main(String [] args) {
 			System.out.println("welcome to address book");
+			AddressBook addressBook = new AddressBook();
+		    addressBook.addContacts();
+		    addressBook.display();
 		}
 	}
 

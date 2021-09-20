01a7a2d5d61c1c77d46b1a9aae5a832b8022fdb8
shruthishruu05 <shruthi.m@perfios.com>
2021-09-20 23:30:47 +0530

    REFACTORED : ability to call write and read method

--- src/main/java/com/bridgelabz/addressbooksystem/AddressBookMain.java
+++ src/main/java/com/bridgelabz/addressbooksystem/AddressBookMain.java
@@ -1,8 +1,13 @@
 package com.bridgelabz.addressbooksystem;
 
+import java.io.IOException;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Scanner;
+
+import com.opencsv.exceptions.CsvDataTypeMismatchException;
+import com.opencsv.exceptions.CsvRequiredFieldEmptyException;
+
 public class AddressBookMain {
 	private static List<AddressBook> addressBooks = new LinkedList<AddressBook>();
 	private static String[] addressBookName = new String[10];
@@ -27,7 +32,9 @@ public class AddressBookMain {
 					+ " 9: sort by name 10:sort by zip 11: sort by city "
 					+ "12: sort by state 13:Switch Address Book"
 					+ "13: Write to file"
-					+ "14. Read from file");
+					+ "14. Read from file"
+					+ "15. Write to CSV"
+					+ "16. Read from CSV");
 			option  = sc.nextInt();
 			switch(option) {
 				case 1 :
@@ -86,6 +93,20 @@ public class AddressBookMain {
 					break;
 				case 14:
 					List<String> listaddressBook = addressBook.read();
+					break;
+				case 15:
+					try {
+						addressBook.writeDataToCSV();
+	                }catch (IOException | CsvRequiredFieldEmptyException | CsvDataTypeMismatchException e) {
+	                    e.printStackTrace();
+	                }
+					break;
+				case 16:
+					try {
+						addressBook.readDataFromCSV();
+	                }catch (IOException e) {
+	                    e.printStackTrace();
+	                }
 					break;
 					
 				default:
